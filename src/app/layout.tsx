330e73daf947c4cd388fc2ba66272e26765bdf4f
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-06-06 03:30:51 +0700

    feat: add toaster in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import VotingLayout from "@/components/voting/VotingLayout";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <>
           <VotingLayout>{children}</VotingLayout>
         </>
+        <Toaster />
       </body>
     </html>
   );
